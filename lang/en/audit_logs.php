48e11c5f2ffb3a54f0e407852fc76a8684bd9cd0
ahmednour1234 <ahmednour5999@gmail.com>
2026-01-05 23:33:03 +0200

    ww

---
+++ lang/en/audit_logs.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+    'basic_info' => 'Basic Information',
+    'action' => 'Action',
+    'subject_type' => 'Subject Type',
+    'subject_id' => 'Subject ID',
+    'actor' => 'Actor',
+    'branch' => 'Branch',
+    'user' => 'User',
+    'meta' => 'Meta',
+    'meta_data' => 'Meta Data',
+    'technical_info' => 'Technical Information',
+    'ip' => 'IP Address',
+    'user_agent' => 'User Agent',
+    'created_at' => 'Created At',
+];
+
