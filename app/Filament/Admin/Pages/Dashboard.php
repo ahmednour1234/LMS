c02e3c3cee72388305ff1fe82967a0e17a415ee6
ahmednour1234 <ahmednour5999@gmail.com>
2026-01-05 06:29:50 +0200

    nour

--- app/Filament/Admin/Pages/Dashboard.php
+++ app/Filament/Admin/Pages/Dashboard.php
@@ -14,7 +14,7 @@ class Dashboard extends BaseDashboard
 {
     protected static ?string $navigationIcon = 'heroicon-o-document-chart-bar';
 
-    protected function getWidgets(): array
+    public function getWidgets(): array
     {
         return [
             StatsOverviewWidget::class,
