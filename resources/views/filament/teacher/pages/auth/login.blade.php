f594aef20f90bcde393ae7718823e801f43db872
ahmednour1234 <ahmednour5999@gmail.com>
2026-01-17 23:32:42 +0200

    5656

--- resources/views/filament/teacher/pages/auth/login.blade.php
+++ resources/views/filament/teacher/pages/auth/login.blade.php
@@ -9,7 +9,7 @@
 
         <div class="mt-4 flex flex-col gap-2 sm:flex-row sm:items-center sm:justify-between">
             <a 
-                href="{{ route('filament.teacher.auth.register') }}" 
+                href="{{ route('filament.teacher.pages.register') }}" 
                 class="text-sm text-primary-600 hover:text-primary-700 dark:text-primary-400 dark:hover:text-primary-300"
             >
                 {{ __('Create account') }}
