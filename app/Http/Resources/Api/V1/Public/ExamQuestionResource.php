2af4d560b19a94d09eeaaea6bfff6509b1062bf8
ahmednour1234 <ahmednour5999@gmail.com>
2026-01-22 12:49:18 +0200

    5656

--- app/Http/Resources/Api/V1/Public/ExamQuestionResource.php
+++ app/Http/Resources/Api/V1/Public/ExamQuestionResource.php
@@ -2,18 +2,43 @@
 
 namespace App\Http\Resources\Api\V1\Public;
 
+use App\Support\Traits\HasTranslatableFields;
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ExamQuestionResource extends JsonResource
 {
-    public function toArray($request): array
+    use HasTranslatableFields;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
     {
+        $locale = app()->getLocale();
+
+        $options = null;
+        if ($this->options) {
+            $optionsArray = is_array($this->options) ? $this->options : json_decode($this->options, true) ?? [];
+            $options = array_map(function($option) use ($locale) {
+                // If option is an object/array with a 'text' field, translate the text
+                if (is_array($option) && isset($option['text'])) {
+                    $option['text'] = $this->getTranslatedValue($option['text'], $locale);
+                    return $option;
+                }
+                // If option itself is translatable (object with ar/en), translate it
+                return $this->getTranslatedValue($option, $locale);
+            }, $optionsArray);
+        }
+
         return [
             'id' => $this->id,
             'exam_id' => $this->exam_id,
             'type' => $this->type, // mcq/essay
-            'question' => $this->question,
-            'options' => $this->options, // for mcq
+            'question' => $this->getTranslatedValue($this->question, $locale),
+            'options' => $options, // for mcq - translate each option
             'correct_answer' => $this->correct_answer,
             'points' => (float) $this->points,
             'order' => $this->order,
