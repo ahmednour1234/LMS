1c8b92a850835868688c3d4dbe1ab78c7299590b
ahmednour1234 <ahmednour5999@gmail.com>
2026-03-05 23:39:36 +0200

    qweqw

--- app/Filament/Admin/Resources/ExpenseResource.php
+++ app/Filament/Admin/Resources/ExpenseResource.php
@@ -23,7 +23,7 @@ class ExpenseResource extends Resource
 
     protected static ?string $navigationGroup = 'accounting';
 
-    protected static ?int $navigationSort = 4;
+    protected static ?int $navigationSort = 2;
 
     public static function getNavigationLabel(): string
     {
